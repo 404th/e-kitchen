8e9a177ab63307a56b1cc96b9b14c6f50923a695
run_over <run.over.0001@gmail.com>
2020-12-11 00:55:39 +0500

    ddd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/normalize.css"
-import Cover from './Components/GlobalState'
+import App from './App'
 
 ReactDOM.render(
     <React.StrictMode>
-      <Cover>
-        <App />
-      </Cover>
+      <App />
     </React.StrictMode>
     ,
   document.getElementById('root')
